4d29c0631044f03f6ec4586befcf215dc5aa7c9d
Kevine_dev <kevinarson_1996@yahoo.fr>
2024-05-23 12:51:49 +0200

    fix security authentification Env

--- app/db/firebase.js
+++ app/db/firebase.js
@@ -1,19 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCQQ4HJLT-Gxy14ysXciULY57aP8P9mdic",
-  authDomain: "tsarasera-f501c.firebaseapp.com",
-  projectId: "tsarasera-f501c",
-  storageBucket: "tsarasera-f501c.appspot.com",
-  messagingSenderId: "667198132588",
-  appId: "1:667198132588:web:70059bd66ea4c20a392349",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
-
+console.table(firebaseConfig);
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
